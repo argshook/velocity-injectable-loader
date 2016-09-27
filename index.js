6712ff0f6003b3bb9485b3edd221db1e1afdccdd
argshook <argshook@gmail.com>
2016-09-27 10:26:34 +0300

    fix to not crash when injectables path not given
    ill test it later maybe

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ module.exports = function(source) {
   this.cacheable && this.cacheable();
 
   const injectablesPath = loaderUtils.parseQuery(this.query).inject || '';
-  const templateContext = require(path.resolve(injectablesPath));
+  const templateContext = injectablesPath ? require(path.resolve(injectablesPath)) : {};
   const Engine = require('velocity').Engine;
   const engine = new Engine({template: source});
-  const result = engine.render(injectablesPath ? templateContext : {});
+  const result = engine.render(templateContext);
 
   return 'module.exports = ' + JSON.stringify(result) + ';';
 };
